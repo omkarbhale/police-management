4437057b75f2fc447b4078274fed21dcb88c93c9
HBDragoNite <harshborse3004@gmail.com>
2022-05-27 02:22:43 +0530

    Created entities

--- src/entities/Criminal.java
+++ src/entities/Criminal.java
@@ -1,4 +1,22 @@
 package entities;
 
+import db.Database;
+
+import java.util.Date;
+
 public class Criminal {
+    private int Criminal_id;
+    private String firstname;
+    private String middlename;
+    private String lastname;
+    private Date DOB;
+    private String address;
+    public Criminal( int _Criminal_id,String _firstname,String _middlename,String _lastname,Date _DOB,String _address){
+        Criminal_id=_Criminal_id;
+        firstname=_firstname;
+        middlename=_middlename;
+        lastname=_lastname;
+        DOB=_DOB;
+        address=_address;
+    }
 }
--- src/entities/Police.java
+++ src/entities/Police.java
@@ -3,15 +3,24 @@ package entities;
 import java.sql.Date;
 
 public class Police {
+    private int Police_id;
     private String firstName;
     private String middleName;
     private String lastName;
     private String post;
-    private Date date;
+    private Date DOB;
     private String contactNo;
     private String address;
 
-    public Police() {
+    public Police(int _Police_id,String _firstname,String _middlename,String _lastname,String _post,Date _DOB,String _contactNo,String _address) {
+            Police_id= _Police_id;
+            firstName=_firstname;
+            middleName=_middlename;
+            lastName=_lastname;
+            post=_post;
+            DOB=_DOB;
+            contactNo=_contactNo;
+            address=_address;
 
     }
 }
