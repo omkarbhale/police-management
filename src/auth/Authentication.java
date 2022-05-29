d2140893e7733d5082a2ff5cd436a1954a1486e6
omkar <omkarbhale001@gmail.com>
2022-05-29 19:43:14 +0530

    Added hashing to passwords

--- src/auth/Authentication.java
+++ src/auth/Authentication.java
@@ -6,6 +6,9 @@ import db.query.SelectQuery;
 import ui.SceneManager;
 import ui.scene.LoginScene;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -79,6 +82,24 @@ public class Authentication {
     }
 
     private String hash(String str) {
-        return str;
+        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = messageDigest.digest();
+            return bytesToHex(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
